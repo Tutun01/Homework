fa2279c614501692153d251f6c99c55d1600dcbb
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-04-24 09:24:30 +0200

    create $latestProducts, the variable is used to display data from the database

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductsModel;
 use Illuminate\Http\Request;
 
 class HomepageController extends Controller
@@ -9,8 +10,10 @@ class HomepageController extends Controller
     public function index()
     {
         $clock = date("h");
-
+        $isMorning = ($clock >= 0 && $clock <= 12);
         $currentTime = date("h:i:s");
-        return view('welcome', compact('currentTime', 'clock'));
+        $latestProducts = ProductsModel::latestProducts();
+
+        return view('welcome', compact('clock', 'isMorning', 'latestProducts', 'currentTime'));
     }
 }
