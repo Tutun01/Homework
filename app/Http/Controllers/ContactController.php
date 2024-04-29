d134eb840079b36f974f2802ba2c53960be9f25b
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-04-29 12:13:17 +0200

    adding data validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,4 +18,23 @@ class ContactController extends Controller
         $allContacts = ContactModel::all(); // SELECT * FROM contacts
         return view("allContacts", compact('allContacts'));
     }
+
+    public function sendContact(Request $request )
+    {
+        $request->validate([
+            "email" => "required|string", // if (isset($_POST['email'])) && is_string($_POST['email'])
+            "subject" => "required|string",
+            "description" => "required|string|min:5"
+         ]);
+
+
+        ContactModel::create([
+            "email" => $request->get("email"),
+            "subject" => $request->get("subject"),
+            "message" => $request->get("description")
+         ]);
+
+        return redirect("/shop");
+
+    }
 }
