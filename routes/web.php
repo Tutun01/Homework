e4977dd23ea9a50c7d3973d75b582f1470674865
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-07 10:36:42 +0200

    Create new routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@ Route::get("/contact", [\App\Http\Controllers\ContactController::class, 'index']
 Route::get("/admin/all-contacts", [\App\Http\Controllers\ContactController::class, "getAllContacts"]);
 
 Route::post("/send-contact", [\App\Http\Controllers\ContactController::class, 'sendContact']);
+
+Route::get("/admin/add-product", [\App\Http\Controllers\ProductsController::class, 'index']);
+
+Route::post("/add-product", [\App\Http\Controllers\ProductsController::class, 'addProduct']);
+
+Route::get("/admin/products", [\App\Http\Controllers\ProductsController::class, 'getAllProducts']);
+
