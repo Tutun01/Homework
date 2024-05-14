3bac0383373ec2f818b201b3c2b955bb2de5bbc6
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-14 11:21:55 +0200

    create new Route "/admin/delete-contact/{contact}"

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,15 @@ Route::get("/admin/all-contacts", [\App\Http\Controllers\ContactController::clas
 
 Route::post("/send-contact", [\App\Http\Controllers\ContactController::class, 'sendContact']);
 
+Route::get("/admin/delete-contact/{contact}", [\App\Http\Controllers\ContactController::class, "delete"]);
+
 Route::get("/admin/add-product", [\App\Http\Controllers\ProductsController::class, 'index']);
 
 Route::post("/add-product", [\App\Http\Controllers\ProductsController::class, 'addProduct']);
 
 Route::get("/admin/products", [\App\Http\Controllers\ProductsController::class, 'getAllProducts']);
+
+Route::get("/admin/all-products", [\App\Http\Controllers\ProductController::class, 'index']);
+
+Route::get("/admin/delete-product/{product}", [\App\Http\Controllers\ProductController::class, "delete"]);
 
