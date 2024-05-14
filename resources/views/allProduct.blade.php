3cf03f0b1fd0618548a9c8d3648b591ca4bb7fa1
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-14 11:23:15 +0200

    edit page

---
+++ resources/views/allProduct.blade.php
@@ -0,0 +1,34 @@
+@extends("layout")
+
+@section("pageContent")
+
+
+
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col">Price</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($allProducts as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->description }}</td>
+            <td>{{ $product->price }}</td>
+            <td>
+                <a href="/admin/delete-product/{{$product->id }}" class="btn btn-danger">Delete</a>
+                <a class="btn btn-primary">Edit</a>
+
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+@endsection
+
