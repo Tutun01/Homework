de8b3d41bc1cf748176a6fed249f151a7b0dbc62
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-14 11:20:49 +0200

    edit page, create table and adding actions

--- resources/views/allContacts.blade.php
+++ resources/views/allContacts.blade.php
@@ -1,4 +1,33 @@
-@foreach($allContacts as $contact)
-    {{ $contact->email }}
+@extends("layout")
 
-@endforeach
+
+
+@section("pageContent")
+
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Email</th>
+        <th scope="col">Subject</th>
+        <th scope="col">Message</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        @foreach($allContacts as $contact)
+            <tr>
+           <td>{{ $contact->id }}</td>
+            <td>{{ $contact->email }}</td>
+            <td>{{ $contact->subject }}</td>
+            <td>{{ $contact->message }}</td>
+            <td>
+                <a href="/admin/delete-contact/{{$contact->id }}" class="btn btn-danger">Delete</a>
+                <a class="btn btn-primary">Edit</a>
+            </td>
+        @endforeach
+    </tr>
+    </tbody>
+</table>
+@endsection
