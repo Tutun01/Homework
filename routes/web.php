fb33acb27e6a14c75bf3e7ae85b7bf723d4d7e84
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-16 09:09:16 +0200

    adding new routes

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,24 @@ Route::get("/admin/all-contacts", [\App\Http\Controllers\ContactController::clas
 
 Route::post("/send-contact", [\App\Http\Controllers\ContactController::class, 'sendContact']);
 
-Route::get("/admin/delete-contact/{contact}", [\App\Http\Controllers\ContactController::class, "delete"]);
+Route::get("/admin/delete-contact/{contact}", [\App\Http\Controllers\ContactController::class, "delete"])
+    ->name("deleteContact");
 
 Route::get("/admin/add-product", [\App\Http\Controllers\ProductsController::class, 'index']);
 
-Route::post("/add-product", [\App\Http\Controllers\ProductsController::class, 'addProduct']);
+Route::post("/add-product", [\App\Http\Controllers\ProductsController::class, 'addProduct'])
+    ->name("saveProduct");
+//Route::get("/admin/products", [\App\Http\Controllers\ProductsController::class, 'getAllProducts']);
 
-Route::get("/admin/products", [\App\Http\Controllers\ProductsController::class, 'getAllProducts']);
+Route::get("/admin/all-products", [\App\Http\Controllers\ProductController::class, 'index'])
+    ->name("allProducts");
 
-Route::get("/admin/all-products", [\App\Http\Controllers\ProductController::class, 'index']);
+Route::get("/admin/delete-product/{product}", [\App\Http\Controllers\ProductController::class, "delete"])
+    ->name("deleteProduct");
 
-Route::get("/admin/delete-product/{product}", [\App\Http\Controllers\ProductController::class, "delete"]);
+Route::get("/admin/product/edit/{id}", [\App\Http\Controllers\ProductController::class, "singleProduct"])
+    ->name("product.single");
+
+Route::post("/admin/product/save/{id}", [\App\Http\Controllers\ProductController::class, "edit"])
+    ->name("product.save");
 
