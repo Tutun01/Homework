35e90a1fa7bd87db65e94d09f885cfafa91615e7
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2024-05-16 09:12:01 +0200

    setting up a new action, adding error function

--- resources/views/add-product.blade.php
+++ resources/views/add-product.blade.php
@@ -5,20 +5,29 @@
 @endsection
 
 @section("pageContent")
-<form method="POST" action="/add-product">
+<form method="POST" action="{{ route("saveProduct") }}">
 
     @if($errors->any())
         <p>Error: {{ $errors->first() }} </p>
     @endif
 
     {{ csrf_field() }}
-        <input name="name"  type="text" placeholder="Enter name">
-        <textarea name="description"></textarea>
-        <input name="amount" type="number" placeholder="Enter amount">
-        <input name="price" type="number" placeholder="Enter price">
-        <input name="image" type="text" placeholder="Enter image">
-        <input name="created_at" type="date" placeholder="Enter the creation date">
-        <input name="updated_at" type="date" placeholder="Enter the update date">
+
+        <div>
+            @if($errors->any())
+                @foreach($errors->all() as $error)
+                    <p class="text-danger">{{$error}}</p>
+                @endforeach
+            @endif
+        </div>
+
+        <input name="name"  type="text" placeholder="Enter name" value = "{{old ("name")}}">
+        <textarea name="description" placeholder="Enter description" ></textarea>
+        <input name="amount" type="number" placeholder="Enter amount" value = "{{old ("amount")}}">
+        <input name="price" type="number" placeholder="Enter price" value = "{{old ("price")}}">
+        <input name="image" type="text" placeholder="Enter image" value = "{{old ("image")}}">
+        <input name="created_at" type="date" placeholder="Enter the creation date" value = "{{old ("created_at")}}">
+        <input name="updated_at" type="date" placeholder="Enter the update date" value = "{{old ("updated_at")}}">
     <button>Add-product</button>
 </form>
 @endsection
