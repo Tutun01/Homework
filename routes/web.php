0add30d8194feb4097df11cc3b65c997e2c63b16
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-06-28 19:09:04 +0200

    * Creating controller groups

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,25 @@ Route::get("/shop", [ShopController::class, 'index']);
 Route::middleware(["auth", AdminCheckMiddleware::class])->prefix("/admin")->group(function (){
 
     Route::controller(ContactController::class)->prefix("/contact")->group(function () {
-        Route::get("/contact", "index");
+        Route::get("/contact", "index")->name("contact");
         Route::get("/all", "getAllContacts");
         Route::get("/delete/{contact}", "delete")->name("deleteContact");
         Route::post("/send", "sendContact")->name("sendContact");
     });
 
-    Route::controller(ProductsController::class)->group(function (){
-        Route::get("/add-product", "index");
-        Route::post("/add-product", "addProduct")->name("saveProduct");
-        Route::get("/all-products", "index")->name("allProducts");
-        Route::get("/delete-product/{product}", "delete")->name("deleteProduct");
-        Route::get("/product/edit/{product}", "singleProduct")  ->name("product.single");
-        Route::post("/product/save/{product}", "edit")->name("product.save");
+    Route::controller(ProductsController::class)->prefix("/products")->group(function (){
+        Route::get("/add-p", "addProducts")->name("addProducts");
+        Route::post("/add", "addProduct")->name("saveProduct");
+
     });
+
+    Route::controller(ProductController::class)->group(function () {
+        Route::get("/all",'index')->name("allProducts");
+        Route::get("/delete/{product}","delete")->name("deleteProduct");
+        Route::get("/edit/{product}","singleProduct")->name("product.single");
+        Route::post("/save/{product}","edit")->name("product.save");
+    });
+
 });
 
 require __DIR__.'/auth.php';
